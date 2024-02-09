6f5ee05f1319dc4b7cf5e63bfac48e66111f7102
rehanshoukat14 <rehanshoukat4558@gmail.com>
2024-02-09 16:59:36 +0500

    2nd commit

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
-import { asyncHandler } from "../utils/asyncHandler";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const varifyJWT =asyncHandler( async (req, res, next) => {
-    req.cookies?.accessToken || req.header("Authorization")?
+    req.cookies?.accessToken || req.header("Authorization")
 })
